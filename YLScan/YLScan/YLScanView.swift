e978d603a6319068d7369c5ccf7b3a90bcd89668
dev-wangliugen <253192463@qq.com>
2017-01-15 21:06:25 +0800

    [update]scanview

---
+++ YLScan/YLScan/YLScanView.swift
@@ -0,0 +1,21 @@
+//
+//  YLScanView.swift
+//  YLScan
+//
+//  Created by 王留根 on 17/1/15.
+//  Copyright © 2017年 ios-mac. All rights reserved.
+//
+
+import UIKit
+
+class YLScanView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
